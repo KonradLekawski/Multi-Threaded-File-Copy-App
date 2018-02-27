113725a68fd59e6fc2b25873722ac602b000a959
KonradLekawski <Snajperv52@gmail.com>
2018-02-27 10:27:28 +0100

    crate folder structure

---
+++ src/main/java/com/codecool/krk/pmk/App.java
@@ -0,0 +1,13 @@
+package com.codecool.krk.pmk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
