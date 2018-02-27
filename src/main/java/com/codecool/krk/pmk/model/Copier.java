aeb0d71b3cd351c2de4bb21a5a6f6b17cfcbf991
KonradLekawski <Snajperv52@gmail.com>
2018-02-27 11:28:25 +0100

    implement Copier with run() method

---
+++ src/main/java/com/codecool/krk/pmk/model/Copier.java
@@ -0,0 +1,32 @@
+package com.codecool.krk.pmk.model;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Copier implements Runnable {
+    private InputStream inputStream;
+    private OutputStream outputStream;
+
+    public Copier(InputStream inputStream, OutputStream outputStream) {
+        this.inputStream = inputStream;
+        this.outputStream = outputStream;
+    }
+
+
+    public void run() {
+
+        try {
+
+            int c;
+            while((c = inputStream.read()) != -1) {
+                outputStream.write(c);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+
+}
