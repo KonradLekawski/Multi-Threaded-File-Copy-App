4081366d7d9c43080fc6e285df9e08bbf752b0d5
KonradLekawski <Snajperv52@gmail.com>
2018-03-06 15:16:21 +0100

    change controller impl using enum and view

--- src/main/java/com/codecool/krk/pmk/controller/CopierController.java
+++ src/main/java/com/codecool/krk/pmk/controller/CopierController.java
@@ -1,22 +1,32 @@
 package com.codecool.krk.pmk.controller;
 
 
+import com.codecool.krk.pmk.exception.ExitApp;
+import com.codecool.krk.pmk.exception.NotTerminatedException;
 import com.codecool.krk.pmk.service.CopierService;
-
+import com.codecool.krk.pmk.view.CopierView;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class CopierController {
     private CopierService service;
+    private CopierView view;
 
-
-    public CopierController(CopierService service) {
+    public CopierController(CopierService service, CopierView view) {
         this.service = service;
+        this.view = view;
     }
 
+    public void start() throws FileNotFoundException, ExitApp, NotTerminatedException {
 
-    public void start(Scanner scanner) throws FileNotFoundException {
-        service.start(scanner);
+        switch (view.getMenuChoose()){
+            case START:
+                service.start();
+                break;
+
+            case STOP:
+                service.end();
+                break;
+        }
     }
 }
 
