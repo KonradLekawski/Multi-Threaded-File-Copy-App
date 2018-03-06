78bd267b895e24dd07f727ea9480910817245f04
KonradLekawski <Snajperv52@gmail.com>
2018-03-06 15:16:50 +0100

    implement basic copier version

--- src/main/java/com/codecool/krk/pmk/App.java
+++ src/main/java/com/codecool/krk/pmk/App.java
@@ -1,15 +1,17 @@
 package com.codecool.krk.pmk;
 
 import com.codecool.krk.pmk.controller.CopierController;
+import com.codecool.krk.pmk.exception.ExitApp;
+import com.codecool.krk.pmk.exception.NotTerminatedException;
 import com.codecool.krk.pmk.model.Copier;
 import com.codecool.krk.pmk.service.CopierService;
 import com.codecool.krk.pmk.view.CopierView;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.util.Scanner;
+import java.util.concurrent.*;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
 
 /**
  * Hello world!
@@ -17,18 +19,24 @@ import java.util.Scanner;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
 
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(4, 4,
+                50, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>());
         Scanner scanner = new Scanner(System.in);
-        CopierService service = new CopierService();
-        CopierController controller = new CopierController(service);
+        CopierView view = new CopierView(scanner);
+        CopierService service = new CopierService(view,executor);
+        CopierController controller = new CopierController(service, view);
 
         while (true) {
             try {
-                controller.start(scanner);
+                controller.start();
             } catch (FileNotFoundException e) {
                 System.out.println("File not found");
+            } catch (NotTerminatedException e) {
+                System.out.println("wait until all operations done");
+            } catch (ExitApp e) {
+                break;
             }
         }
     }
