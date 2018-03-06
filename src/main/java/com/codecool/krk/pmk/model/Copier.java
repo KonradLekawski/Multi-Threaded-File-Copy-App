31ab2a59122f292e8f9b3c13beb2959e20338bf1
KonradLekawski <Snajperv52@gmail.com>
2018-03-06 15:14:30 +0100

    remove all ui from copier model

--- src/main/java/com/codecool/krk/pmk/model/Copier.java
+++ src/main/java/com/codecool/krk/pmk/model/Copier.java
@@ -1,7 +1,5 @@
 package com.codecool.krk.pmk.model;
 
-import com.codecool.krk.pmk.view.CopierView;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,70 +10,61 @@ public class Copier implements Runnable {
     private Integer size;
     private int portionSize;
     private int sumPortion;
-    private CopierView copierView;
 
 
-    public Copier(InputStream inputStream, OutputStream outputStream, CopierView view) {
+    public Copier(InputStream inputStream, OutputStream outputStream) {
         this.inputStream = inputStream;
         this.outputStream = outputStream;
         this.portionSize = 512;
         this.sumPortion = 0;
-        this.copierView = view;
     }
 
     public void run() {
 
         try {
             size = inputStream.available();
-            System.out.println(size);
             copyPortion();
             copyRest();
 
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
 
     }
 
-    private void copyPortion() throws IOException {
+    private void copyPortion() throws IOException, InterruptedException {
 
         int portionAmount = size / portionSize;
-        System.out.println(portionAmount);
-
         byte[] portion = new byte[portionSize];
 
         for (int i = 0; i < portionAmount; i ++) {
             inputStream.read(portion);
             outputStream.write(portion);
             sumPortion += portionSize;
-            this.buildStatusBar(this.countPercent());
+//            this.buildStatusBar(this.countPercent());
         }
 
     }
 
-    private void copyRest() throws  IOException {
+    private void copyRest() throws  IOException, InterruptedException {
         byte[] rest = new byte[size % portionSize];
 
         if(rest.length > 0) {
             inputStream.read(rest);
             outputStream.write(rest);
             sumPortion += size % portionSize;
-            this.buildStatusBar(this.countPercent());
+//            this.buildStatusBar(this.countPercent());
         }
     }
 
-    private void buildStatusBar(double progressPercentage) {
-        try {
-            copierView.updateProgress(progressPercentage);
-            Thread.sleep(20);
-
-        } catch (InterruptedException e) {
-
-        }
-    }
+//    private void buildStatusBar(double progressPercentage) throws InterruptedException {
+//
+//        copierView.updateProgress(progressPercentage);
+//        Thread.sleep(20);
+//
+//    }
 
     private double countPercent() {
-
         double countPercent = (double) sumPortion/size;
         return Math.round(countPercent*100.0)/100.0;
     }
